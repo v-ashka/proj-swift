da3b08ffaa5c462f8387f4aafbcfef9b913f5b65
vashka <mwijaszka99@gmail.com>
2024-01-03 11:55:31 -0800

    Initial Commit

---
+++ idleclicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  idleclicker
+//
+//  Created by vashka on 03/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ idleclicker/idleclickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  idleclickerApp.swift
+//  idleclicker
+//
+//  Created by vashka on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct idleclickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
